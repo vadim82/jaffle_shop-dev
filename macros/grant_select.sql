90ea1e44866cbcb4418ffdc0ce1776a2f6072f29
Vadim Rybak <vadim.rybak@dbtlabs.com>
2021-12-10 10:41:59 -0500

    fixed broken test

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,10 +1,10 @@
 {% macro grant_select(role) %}
-{% set sql %}
-	grant usage on schema {{ target.schema }} to {{ role }};    
-	grant select on all tables in schema {{ target.schema }} to {{ role }};    
-	grant select on all views in schema {{ target.schema }} to {{ role }};
-{% endset %}
+-- {% set sql %}
+-- 	grant usage on schema {{ target.schema }} to {{ role }};    
+-- 	grant select on all tables in schema {{ target.schema }} to {{ role }};    
+-- 	grant select on all views in schema {{ target.schema }} to {{ role }};
+-- {% endset %}
 
-{% do run_query(sql) %}
+-- {% do run_query(sql) %}
 {% do log("Privileges granted", info=True) %}
 {% endmacro %}
