809ac1b2fcf8916135cdce34185596c9a65f6d37
Vadim Rybak <vadim.rybak@dbtlabs.com>
2021-12-10 10:32:00 -0500

    fixed macro

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,8 +1,8 @@
 {% macro grant_select(role) %}
 {% set sql %}
-	grant usage on schema {{ target.schema }} to role {{ role }};    
-	grant select on all tables in schema {{ target.schema }} to role {{ role }};    
-	grant select on all views in schema {{ target.schema }} to role {{ role }};
+	grant usage on schema {{ target.schema }} to {{ role }};    
+	grant select on all tables in schema {{ target.schema }} to {{ role }};    
+	grant select on all views in schema {{ target.schema }} to {{ role }};
 {% endset %}
 
 {% do run_query(sql) %}
